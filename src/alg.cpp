977878d38e0f664bf371749fb324d665c479e7da
M4xSoloviev <79105782+M4xSoloviev@users.noreply.github.com>
2022-06-11 05:37:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int result = 0, first = 0, second = size - 1, fr;
+    while (first < second) {
+        fr = (first + second) / 2;
+        if (arr[fr] == value) {
+            first = fr;
+            while (arr[first] == value) {
+                first--;
+            }
+            while (arr[cecond] != value) {
+                second--;
+            }
+            for (++first; first <= second; first++) {
+                result++;
+            }
+        }
+        if (arr[fr] < value) {
+            first = fr + 1;
+        }
+        if (arr[fr] > value) {
+            second = fr - 1;
+        }
+    }
+    if (result == 0) {
+        if (arr[first] == value) {
+            return 1;
+        }
+    }
+    return result;
 }
